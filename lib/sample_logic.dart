d17c2cae76dd3da0ae7dee4fcd1b2320a30b0236
MLLB <app.developer.mllb@gmail.com>
2024-05-11 16:46:45 +0900

    add sample test

---
+++ lib/sample_logic.dart
@@ -0,0 +1,4 @@
+class SampleLogic {
+  String getSampleText() => "Hello, World!";
+  String getGreeting(String name) => "Hello, $name!";
+}
---
+++ test/sample_logic_test.dart
@@ -0,0 +1,15 @@
+import "package:flutter_sample_ci/sample_logic.dart";
+import "package:flutter_test/flutter_test.dart";
+
+void main() {
+  final sut = SampleLogic();
+  test("getSampleText should return Hello, World!", () {
+    final result = sut.getSampleText();
+    expect(result, "Hello, World!");
+  });
+
+  test("getGreeting should return Hello, {name}!", () {
+    final result = sut.getGreeting("Flutter");
+    expect(result, "Hello, Python!");
+  });
+}
