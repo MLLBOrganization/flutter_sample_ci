ce8546407d6ee1409f76be0be7782e841eb1fd38
MLLB <app.developer.mllb@gmail.com>
2024-05-12 22:20:13 +0900

    fix ci

--- test/sample_logic_test.dart
+++ test/sample_logic_test.dart
@@ -3,12 +3,12 @@ import "package:flutter_test/flutter_test.dart";
 
 void main() {
   final sut = SampleLogic();
-  test("getSampleText should return Hello, World!", () {
+  test("Unit: getSampleText should return Hello, World!", () {
     final result = sut.getSampleText();
     expect(result, "Hello, World!");
   });
 
-  test("getGreeting should return Hello, {name}!", () {
+  test("Unit: getGreeting should return Hello, {name}!", () {
     final result = sut.getGreeting("Flutter");
     expect(result, "Hello, Flutter!");
   });
